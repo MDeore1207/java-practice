353ae94686e6118a5f78a03609ad3271a27a76ff
Mayuri Deore <110525391+MDeore1207@users.noreply.github.com>
2026-08-05 14:07:28 +0530

    Create TryCatch.java

---
+++ ExceptionHandling/TryCatch.java
@@ -0,0 +1,19 @@
+public class TryCatch {
+
+    public static void main(String[] args){
+
+        try{
+
+            int a=10/0;
+
+            System.out.println(a);
+
+        }catch(Exception e){
+
+            System.out.println(e);
+
+        }
+
+    }
+
+}
