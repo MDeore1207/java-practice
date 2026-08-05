a89be3fdcd4ddab2e4234d1cfd7a2984a12db3cb
Mayuri Deore <110525391+MDeore1207@users.noreply.github.com>
2026-08-05 14:04:35 +0530

    Create HashMapDemo.java

---
+++ Collections/HashMapDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class HashMapDemo {
+
+    public static void main(String[] args){
+
+        HashMap<Integer,String> map=new HashMap<>();
+
+        map.put(1,"Mayuri");
+        map.put(2,"Java");
+
+        System.out.println(map);
+    }
+}
