1938da768fb84d978667de464166eeb1dfacd596
Mayuri Deore <110525391+MDeore1207@users.noreply.github.com>
2026-08-05 13:52:17 +0530

    Create Variables.java

---
+++ Basics/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 23;
+        double salary = 45000.50;
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Java is Fun: " + isJavaFun);
+    }
+}
