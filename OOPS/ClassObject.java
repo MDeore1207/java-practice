96e538f6ed195a23a1ea0f4ed2bdc2d318731d9c
Mayuri Deore <110525391+MDeore1207@users.noreply.github.com>
2026-08-05 13:58:51 +0530

    Create ClassObject.java

---
+++ OOPS/ClassObject.java
@@ -0,0 +1,22 @@
+class Student{
+
+    int id;
+    String name;
+
+    void display(){
+        System.out.println(id+" "+name);
+    }
+}
+
+public class ClassObject {
+
+    public static void main(String[] args){
+
+        Student s1=new Student();
+
+        s1.id=1;
+        s1.name="Mayuri";
+
+        s1.display();
+    }
+}
